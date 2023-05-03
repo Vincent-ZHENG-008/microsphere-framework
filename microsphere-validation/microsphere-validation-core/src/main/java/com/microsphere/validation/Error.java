c570c485d78250926d46503055afbf85103ad84d
WunHwan <wunhwantseng@gmail.com>
2023-05-03 08:18:49 +0800

    - add validation module - add validation common definition

---
+++ microsphere-validation/microsphere-validation-core/src/main/java/com/microsphere/validation/Error.java
@@ -0,0 +1,7 @@
+package com.microsphere.validation;/**
+ * #: todo - what is this
+ * 
+ * @author wunhwantseng@gmail.com 
+ * @since todo - since from which version
+ */public interface Error {
+}
---
+++ microsphere-validation/microsphere-validation-core/src/main/java/com/microsphere/validation/FieldError.java
@@ -0,0 +1,10 @@
+package com.microsphere.validation.error;
+
+/**
+ * #: todo - what is this
+ *
+ * @author wunhwantseng@gmail.com
+ * @since todo - since from which version
+ */
+public class FieldError {
+}
